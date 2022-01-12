57e5e7f8b2c5f94b56f83ec8e82d1e3db5aa9213
vikashkumarsaini1598 <vickykartoos1598@gmail.com>
2022-01-12 14:24:31 +0530

    first commit

---
+++ testapp/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+    def __str__(self):
+        return "%s the place address: %s" % (self.name,self.address)
+
+class Restaurant(models.Model):
+    place = models.OneToOneField(
+        Place,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s ---serves_hot_dogs- %s---serves_pizza- %s" % (self.place.name,self.serves_hot_dogs,self.serves_pizza)
+
+class Waiter(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "%s the waiter at %s" % (self.name, self.restaurant)
